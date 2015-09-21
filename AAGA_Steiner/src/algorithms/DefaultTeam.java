55dbe70beced68c7d3ace994661c2143c6e3a45a
MaximeBonnet27 <maximebonnet27@gmail.com>
2015-09-21 21:10:37 +0200

    Ajouté amélioration barycentre
    Score 3973

--- AAGA_Steiner/src/algorithms/DefaultTeam.java
+++ AAGA_Steiner/src/algorithms/DefaultTeam.java
@@ -2,31 +2,33 @@ package algorithms;
 
 import java.awt.Point;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Random;
 
 public class DefaultTeam {
   public Tree2D calculSteiner(ArrayList<Point> points) {
-    Tree2D res = prim(points);
+    Tree2D res;
+    res = prim(points);
+    barycentres(res);
     return res;
-    
+
   }
-  
-  private Tree2D prim(ArrayList<Point> points){
-    
+
+  private Tree2D prim(ArrayList<Point> points) {
+
     Tree2D resultTree = null;
     Point p = points.get(0);
     ArrayList<Point> treePoints = new ArrayList<>();
     resultTree = new Tree2D(p, new ArrayList<Tree2D>());
     treePoints.add(p);
-    while(treePoints.size() != points.size()){
+    while (treePoints.size() != points.size()) {
       Edge minimumEdge = null;
       double minimumEdgeLength = Double.MAX_VALUE;
-      for(Point treePoint : treePoints){
-        for(Point point : points){
-          if(!treePoints.contains(point)){
-            if(treePoint.distance(point) < minimumEdgeLength){
-               minimumEdge = new Edge(treePoint, point);
-               minimumEdgeLength = minimumEdge.length();
+      for (Point treePoint : treePoints) {
+        for (Point point : points) {
+          if (!treePoints.contains(point)) {
+            if (treePoint.distance(point) < minimumEdgeLength) {
+              minimumEdge = new Edge(treePoint, point);
+              minimumEdgeLength = minimumEdge.length();
             }
           }
         }
@@ -35,38 +37,73 @@ public class DefaultTeam {
       Tree2D newLeaf = new Tree2D(minimumEdge.B, new ArrayList<Tree2D>());
       leaf.getSubTrees().add(newLeaf);
       treePoints.add(minimumEdge.B);
-      
+
     }
-    System.out.println(treePoints.size());
-    System.out.println(score(resultTree));
     return resultTree;
   }
-    
-  private Tree2D findInTree(Tree2D tree, Point p){
-    if(tree == null) return null;
-    if(tree.getRoot().equals(p)){
+
+  private Tree2D findInTree(Tree2D tree, Point p) {
+    if (tree == null)
+      return null;
+    if (tree.getRoot().equals(p)) {
       return tree;
-    }
-    else {
+    } else {
       Tree2D res = null;
-      for(Tree2D sub : tree.getSubTrees()){
-        if((res = findInTree(sub, p)) != null){
+      for (Tree2D sub : tree.getSubTrees()) {
+        if ((res = findInTree(sub, p)) != null) {
           return res;
         }
       }
       return null;
     }
   }
-  
-  private int score(Tree2D tree){
+
+  private int score(Tree2D tree) {
     int res = 0;
     ArrayList<Tree2D> trees = new ArrayList<>();
     trees.add(tree);
-    while(!trees.isEmpty()){
+    while (!trees.isEmpty()) {
       Tree2D current = trees.remove(0);
       res += current.distanceRootToSubTrees();
       trees.addAll(current.getSubTrees());
     }
     return res;
+  }
+
+  private void barycentres(Tree2D tree){
+    ArrayList<Tree2D> trees = new ArrayList<Tree2D>();
+    trees.add(tree);
+    while(!trees.isEmpty()){
+      Tree2D current = trees.remove(0);
+      Point p = current.getRoot();
+      Point q = null;
+      Point r = null;
+      for(int i = 0; i < current.getSubTrees().size(); ++i){
+        Tree2D subTree = current.getSubTrees().get(i); 
+        trees.add(subTree);
+        q = subTree.getRoot();
+        for(int j = 0; j < subTree.getSubTrees().size(); ++j){
+          Tree2D subSubTree = subTree.getSubTrees().get(j);
+          r = subSubTree.getRoot();
+          if(p != null && q != null && r != null &&
+              betterWithBarycentre(p,q,r)){
+            Point barycentre = new Point((int) ((p.x + q.x + r.x) / 3), (int) ((p.y + q.y + r.y) / 3));
+            Tree2D baryTree = new Tree2D(barycentre, new ArrayList<Tree2D>());
+            current.getSubTrees().remove(subTree);
+            current.getSubTrees().add(baryTree);
+            subTree.getSubTrees().remove(subSubTree);
+            baryTree.getSubTrees().add(subTree);
+            baryTree.getSubTrees().add(subSubTree);
+          }
+        }
+      }
+    }
+  }
+
+  private boolean betterWithBarycentre(Point p, Point q, Point r){
+    double regularDistance = p.distance(q) + q.distance(r);
+    Point barycentre = new Point((int) ((p.x + q.x + r.x) / 3), (int) ((p.y + q.y + r.y) / 3));
+    double barycentreDistance = p.distance(barycentre) + barycentre.distance(q) + barycentre.distance(r);
+    return barycentreDistance < regularDistance;
   }
 }
