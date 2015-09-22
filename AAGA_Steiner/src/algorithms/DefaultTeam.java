88a587fb1e030ec0d597a1bb33abd59e1e11cf31
MaximeBonnet27 <maximebonnet27@gmail.com>
2015-09-22 09:11:07 +0200

    Ajouté une autre méthode pour les barycentres
    Score 3923

--- AAGA_Steiner/src/algorithms/DefaultTeam.java
+++ AAGA_Steiner/src/algorithms/DefaultTeam.java
@@ -8,7 +8,14 @@ public class DefaultTeam {
   public Tree2D calculSteiner(ArrayList<Point> points) {
     Tree2D res;
     res = prim(points);
-    barycentres(res);
+    int oldScore, newScore;
+    do{
+      oldScore = score(res);
+      barycentresSubAndSubSub(res);
+      barycentresSub(res);
+      newScore = score(res);
+    }while(newScore < oldScore);
+    System.out.println(score(res));
     return res;
 
   }
@@ -59,7 +66,7 @@ public class DefaultTeam {
   }
 
   private int score(Tree2D tree) {
-    int res = 0;
+    double res = 0;
     ArrayList<Tree2D> trees = new ArrayList<>();
     trees.add(tree);
     while (!trees.isEmpty()) {
@@ -67,10 +74,10 @@ public class DefaultTeam {
       res += current.distanceRootToSubTrees();
       trees.addAll(current.getSubTrees());
     }
-    return res;
+    return (int) res;
   }
 
-  private void barycentres(Tree2D tree){
+  private void barycentresSubAndSubSub(Tree2D tree){
     ArrayList<Tree2D> trees = new ArrayList<Tree2D>();
     trees.add(tree);
     while(!trees.isEmpty()){
@@ -99,6 +106,37 @@ public class DefaultTeam {
       }
     }
   }
+
+  private void barycentresSub(Tree2D tree){
+    ArrayList<Tree2D> trees = new ArrayList<Tree2D>();
+    trees.add(tree);
+    while(!trees.isEmpty()){
+      Tree2D current = trees.remove(0);
+      Point p = current.getRoot();
+      Point q = null;
+      Point r = null;
+      for(int i = 0; i < current.getSubTrees().size(); ++i){
+        Tree2D subTree1 = current.getSubTrees().get(i);
+        trees.add(subTree1);
+        for(int j = i + 1; j < current.getSubTrees().size(); ++j){
+          Tree2D subTree2 = current.getSubTrees().get(j);
+          q = subTree1.getRoot();
+          r = subTree2.getRoot();
+          if(p != null && q != null && r != null &&
+              betterWithBarycentre(q,p,r)){
+            Point barycentre = new Point((int) ((p.x + q.x + r.x) / 3), (int) ((p.y + q.y + r.y) / 3));
+            Tree2D baryTree = new Tree2D(barycentre, new ArrayList<Tree2D>());
+            current.getSubTrees().remove(subTree1);
+            current.getSubTrees().remove(subTree2);
+            baryTree.getSubTrees().add(subTree1);
+            baryTree.getSubTrees().add(subTree2);
+            current.getSubTrees().add(baryTree);
+          }
+        }
+      }
+    }
+  }
+  
 
   private boolean betterWithBarycentre(Point p, Point q, Point r){
     double regularDistance = p.distance(q) + q.distance(r);
